08d117904f6f1c968016c16576d6872d3099c38f
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2025-08-26 16:52:44 +0200

    Add files via upload

---
+++ Variabili.jsp/variabili.js
@@ -0,0 +1,28 @@
+let num1;
+let num2;
+
+num1=4;
+num2=3;
+
+console.log(num1);
+console.log(num2);
+
+let saluto ='ciao';
+
+console.log(saluto);
+
+saluto='salve';
+
+console.log(saluto);
+
+const PIGRECO= 3.14159265359;
+
+
+
+
+console.log(PIGRECO);
+
+PIGRECO=3.50;
+
+console.log(PIGRECO);
+
