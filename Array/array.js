6204923bedc971dae5759a02050e390968276518
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2025-09-29 18:32:30 +0200

    Add files via upload

--- Array/array.js
+++ Array/array.js
@@ -1,3 +1,4 @@
+/*
 let array_1 =  ['un', 'per', 'incatenarli','Anello', 'trovarli','ghermirli', 'e','gondor', 'mark'];
 
 
@@ -7,3 +8,44 @@ let array_2 = ['trovarli,','tu', 'sciocchi','tu', 'sciocchi', 'padron', 'Sauron'
 
 console.log(array_1[0],array_1[3],array_1[1],array_2[10],array_1[0],array_1[3],array_1[1],array_2[0]
     ,array_1[0],array_1[3],array_1[1],array_1[5],array_1[6],array_2[7],array_2[12],array_1[2]);
+
+*/
+
+
+
+let tiri= Number(prompt('Inserisci il numero di tiri da fare'));
+let utente1;
+let utente2;
+
+
+
+let punteggioutente1=0;
+let punteggioutente2=0;
+
+
+
+
+for(i=1;i<=tiri;i++){
+    let tiro1 = Math.floor(Math.random() * 6) + 1;
+    let tiro2 = Math.floor(Math.random() * 6) + 1;
+
+    console.log(`Tiro ${i}: Utente 1 = ${tiro1}, Utente 2 = ${tiro2}`);
+
+    punteggioutente1 += tiro1;
+    punteggioutente2 += tiro2;
+
+
+
+}
+
+console.log(`\nPunteggio finale:`);
+console.log(`Utente 1: ${punteggioutente1}`);
+console.log(`Utente 2: ${punteggioutente2}`);
+
+if (punteggioutente1 > punteggioutente2) {
+    console.log("Vince Utente 1!");
+} else if (punteggioutente2 > punteggioutente1) {
+    console.log("Vince Utente 2!");
+} else {
+    console.log("Pareggio!");
+}
