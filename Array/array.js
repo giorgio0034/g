1a54810892d6e9724fe955f421d8b0c3ebbe65b0
giorgio0034 <152870852+giorgio0034@users.noreply.github.com>
2025-09-29 17:20:11 +0200

    Add files via upload

---
+++ Array/array.js
@@ -0,0 +1,9 @@
+let array_1 =  ['un', 'per', 'incatenarli','Anello', 'trovarli','ghermirli', 'e','gondor', 'mark'];
+
+
+
+let array_2 = ['trovarli,','tu', 'sciocchi','tu', 'sciocchi', 'padron', 'Sauron','nel', 'fuggite', 'gandalf','domarli,', 'passare', 'buio'];
+
+
+console.log(array_1[0],array_1[3],array_1[1],array_2[10],array_1[0],array_1[3],array_1[1],array_2[0]
+    ,array_1[0],array_1[3],array_1[1],array_1[5],array_1[6],array_2[7],array_2[12],array_1[2]);
